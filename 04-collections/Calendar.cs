a8da86ca90e84d6503a83de53d1ac1b5a5a7d41b
Denis Sokolov <denis.sokolov@abbyy.com>
2022-03-17 21:55:12 +0300

    add seminar 4, 5

---
+++ 04-collections/Calendar.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+
+public class Activity
+{
+    public string Name { get; set; }
+
+    public DateTime Timestamp { get; set; }
+}
+
+public class Calendar : IEnumerable
+{
+    private readonly Activity[] _activities;
+
+    public Calendar(params Activity[] activities)
+    {
+        _activities = activities;
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        return new CalendarEnumerator(_activities);
+    }
+}
+
+public class CalendarEnumerator : IEnumerator
+{
+    private readonly Activity[] _activities;
+    private int _position = -1;
+
+    public CalendarEnumerator(Activity[] activities)
+    {
+        _activities = activities
+            .OrderBy(activity => activity.Timestamp)
+            .ToArray();
+    }
+
+    public bool MoveNext()
+    {
+        if (_position < _activities.Length - 1)
+        {
+            _position++;
+            return true;
+        }
+        return false;
+    }
+
+    public void Reset()
+    {
+        _position = -1;
+    }
+
+    public object Current
+    {
+        get
+        {
+            if (_position == -1 || _position >= _activities.Length)
+                throw new ArgumentException();
+            return _activities[_position];
+        }
+    }
+}
