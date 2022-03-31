3ef6836b26edec6ad3b579def774c1bb562b34f2
Lystsova Lubov <lystsova.la@phystech.edu>
2022-03-31 19:10:10 +0300

    task2

--- 05-multithreading/Program.cs
+++ 05-multithreading/Program.cs
@@ -17,3 +17,137 @@
     Мягкий дедлайн: 31.03.2022 23:59
     Жесткий дедлайн: 12.05.2022 23:59
 */
+//using System.Linq;
+//using System.Text.RegularExpressions;
+using System;
+using System.Xml;
+using System.Net;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Syndication;
+using System.Collections.Concurrent;
+
+class ProcessedPages {
+    private ConcurrentDictionary<string, bool> processed_articles;
+    private SemaphoreSlim guard;
+
+    StreamWriter writer;
+
+    public ProcessedPages(string file) {
+        this.guard = new SemaphoreSlim(1, 1);
+        this.processed_articles = new ConcurrentDictionary<string, bool>();
+        this.writer = new StreamWriter(file, true);
+
+        StreamReader reader = new StreamReader(file);
+
+        string? line;
+        while ((line = reader.ReadLine()) != null)
+        {
+            this.processed_articles.TryAdd(line, true);
+        }
+    }
+
+    async public Task<bool> Update(string url) {
+        if (this.processed_articles.TryAdd(url, true)) {
+            await this.guard.WaitAsync();
+            try
+            {
+                await this.writer.WriteLineAsync(url);
+                await this.writer.FlushAsync();
+            }
+            finally
+            {
+                this.guard.Release();
+            }
+            return true;
+        }
+        return false;
+    }
+}
+
+
+class Program
+{
+    static private readonly string rss_list = "rss_list.txt";
+    static private readonly string processed_file = "processed_articles.txt";
+    static private readonly string news_folder = "news";
+
+    static int total_count;
+    static int downloaded;
+    static private ProcessedPages processed = new ProcessedPages(Program.processed_file);
+    public static void Main()
+    {
+        if (!Directory.Exists(Program.news_folder))
+        {
+            Directory.CreateDirectory(Program.news_folder);
+        }
+
+        while(true) {
+            Program.total_count = 0;
+            Program.downloaded = 0;
+            Task.Run(() => Program.UpdateAll()).Wait();
+            Console.WriteLine($"total pages: {Program.total_count}, downloaded now: {Program.downloaded}, previously downloaded: {Program.total_count - Program.downloaded}");
+            Console.WriteLine("Iteratoin fineshed, sleep for 5s");
+            Console.WriteLine("");
+            Thread.Sleep(5000);
+
+        }
+    }
+
+
+    static async public Task UpdateAll()
+    {
+        StreamReader reader = new StreamReader(Program.rss_list);
+
+        string? line;
+        List<Task> tasks = new List<Task>();
+        while ((line = reader.ReadLine()) != null)
+        {
+            tasks.Add(Program.UpdateFeed(line));
+        }
+        Task.WaitAll(tasks.ToArray());
+    }
+
+    static async public Task UpdateFeed(string feed_url)
+    {
+        Console.WriteLine($"Processing {feed_url} feed");
+
+        List<Task> tasks = new List<Task>();
+        XmlReader reader = XmlReader.Create(feed_url);
+        SyndicationFeed feed = await Task.Run(() => SyndicationFeed.Load(reader));
+        reader.Close();
+        foreach (SyndicationItem item in feed.Items)
+        {
+            tasks.Add(Program.ProcessPage(item.Links.First().Uri.ToString()));
+        }
+
+        Task.WaitAll(tasks.ToArray());
+        Console.WriteLine($"Finished processing {feed_url} feed");
+    }
+
+    static async public Task ProcessPage(string page_url)
+    {
+        Program.total_count += 1;
+        if (!await Program.processed.Update(page_url)) {
+            return;
+        } 
+        Console.WriteLine($"Page {page_url} dowloading");
+        try
+        {
+            using (WebClient client = new WebClient())
+            {
+                var file = $"{Program.news_folder}/{page_url.Replace("/", "-").Replace(".html", "")}.html";
+                await client.DownloadFileTaskAsync(page_url, file);
+            }
+            Program.downloaded += 1;
+            Console.WriteLine($"Page {page_url} downloaded");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error whiele processing page {page_url}: {e.Message}");
+        }
+
+    }
+}
