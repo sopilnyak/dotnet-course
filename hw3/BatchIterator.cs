ee14f668a2eed95917eb2d4a009cd60b9fef0281
vomiz9k <kirill@kyazimov.ru>
2022-04-17 01:14:14 +0300

    hw3 ready

---
+++ hw3/BatchIterator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw3
+{
+    public class BatchIterator<T>
+    {
+        private IEnumerable<T> data;
+        private int batch_size;
+        public BatchIterator(IEnumerable<T> data, int batch_size)
+        {
+            this.data = data;
+            this.batch_size = batch_size;
+        }
+
+        public IEnumerable<T[]> GetEnumerator()
+        {
+            T[] batch = new T[batch_size];
+            int count = 0;
+            foreach (T item in data)
+            {
+                batch[count++] = item;
+                if (count == batch_size)
+                {
+                    yield return batch;
+                    batch = new T[batch_size];
+                    count = 0;
+                }
+            }
+            if (count > 0)
+            {
+                yield return batch[0..count];
+            }
+            yield break;
+        }
+
+
+    }
+    public static class BatchIteratorGetter
+    {
+        public static BatchIterator<T> ToBatchIterator<T>(this IEnumerable<T> data, int batch_size)
+        {
+            return new BatchIterator<T>(data, batch_size);
+        }
+    }
+}
+
+
---
+++ hw3/BatchIteratorTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using hw3;
+
+public class BatchIteratorTest {
+    [TestCase(10, 1)]
+    [TestCase(10, 3)]
+    [TestCase(10, 5)]
+    [TestCase(10, 7)]
+    [TestCase(100, 10)]
+    [TestCase(100, 49)]
+    [TestCase(100, 75)]
+    [TestCase(1000, 1)]
+    [TestCase(1000, 101)]
+    [TestCase(1000, 500)]
+    [TestCase(1000, 750)]
+    [TestCase(1000000, 1001)]
+    public void TestInt(int size, int batch_size)
+    {
+        int[] arr = new int[size];
+        for(int j = 0; j <size; ++j)
+        {
+            arr[j] = j;
+        }
+        IEnumerable<int> enumerable = arr;
+        var batch_iter = enumerable.ToBatchIterator(batch_size).GetEnumerator();
+        int curr_idx = 0;
+
+        foreach(var batch in batch_iter)
+        {
+            int curr_batch_size = 0;
+            foreach(var item in batch)
+            {
+                Console.WriteLine(arr[curr_idx].ToString() + " " + item.ToString());
+                Assert.AreEqual(arr[curr_idx++], item);
+                curr_batch_size++;
+            }
+            Assert.True(curr_batch_size == batch_size || curr_idx == size);
+        }
+        Assert.AreEqual(curr_idx, size);
+    }
+}
