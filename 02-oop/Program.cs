fa10177e49c5d000509906a8220816806aeef585
ArsenMalkh <arsen.malkhasyan@phystech.edu>
2022-05-07 00:34:06 +0300

    fixed

--- 02-oop/Program.cs
+++ 02-oop/Program.cs
@@ -13,224 +13,275 @@
 */
 using System;
 using System.Diagnostics;
+using System.IO;
 
 namespace Homework1
 {
-public class Log
-{
-    private static Stopwatch stopWatch;
-    public static void StartLog<T>(T[] items, string algo_name)
+    public class Log
     {
-        Console.WriteLine($"получен массив из {items.Length} элементов");
-        Console.WriteLine($"выбран алгоритм {algo_name}");
-        stopWatch = new Stopwatch();
-        stopWatch.Start();
-    }
-    public static void EndLog<T>(T[] items)
-    {
-        stopWatch.Stop();
-        TimeSpan ts = stopWatch.Elapsed;
-        string elapsedTime =
-            String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
-        Console.WriteLine($"отсортировано за {elapsedTime} времени");
-        Console.Write('\n');
-    }
-}
-public class SortAlgorithms
-{
-    private static int length;
-    //////////////////////////////////////////
-    private static void Swap<T>(ref T a, ref T b) where T : IComparable
-    {
-        T t = a;
-        a = b;
-        b = t;
-    }
-    private static void MergeSort_Recursive<T>(T[] numbers, T[] temp, int left, int right) where T : IComparable
-    {
-        int mid;
-        if (right > left)
+        private static Stopwatch stopWatch;
+        public static void StartLog<T>(T[] items, string algo_name, bool is_infile)
         {
-            mid = (right + left) / 2;
-            MergeSort_Recursive(numbers, temp, left, mid);
-            MergeSort_Recursive(numbers, temp, (mid + 1), right);
+            string line1 = "получен массив из " + items.Length + " элементов" + "\r\n";
+            string line2 = "выбран алгоритм " + algo_name + "\r\n";
+            if (is_infile) {
+                using(FileStream stream = new FileStream("logs.txt", FileMode.Append)){
+                    byte[] array1 = System.Text.Encoding.Default.GetBytes(line1);
+                    byte[] array2 = System.Text.Encoding.Default.GetBytes(line2);
 
-            DoMerge(numbers, temp, left, (mid + 1), right);
+                    stream.Write(array1, 0, array1.Length);
+                    stream.Flush();
+                    stream.Write(array2, 0, array2.Length);
+                    stream.Flush();
+                }
+            }
+            else
+            {
+                Console.Write(line1);
+                Console.Write(line2);
+            }
+            stopWatch = new Stopwatch();
+            stopWatch.Start();
         }
-        return;
-    }
-    private static void DoMerge<T>(T[] numbers, T[] temp, int left, int mid, int right) where T : IComparable
-    {
-        // Console.WriteLine("cp2 [" + left + ", " + right + "]");
-        int left_end, num_elements, tmp_pos;
-
-        left_end = (mid - 1);
-        tmp_pos = left;
-        num_elements = (right - left + 1);
-
-        while ((left <= left_end) && (mid <= right))
+        public static void EndLog<T>(T[] items, bool is_infile)
         {
-            if (numbers [left]
-                    .CompareTo(numbers[mid]) <= 0)
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = $"{ts.Hours:00}:{ts.Minutes:00}:{ts.Seconds:00}.{ts.Milliseconds / 10:00}";
+            string line1 = "отсортировано за " + elapsedTime  +" времени" + "\r\n\n";
+
+            if (is_infile) {
+                using(FileStream stream = new FileStream("logs.txt", FileMode.Append)){
+                    byte[] array1 = System.Text.Encoding.Default.GetBytes(line1);
+                    stream.Write(array1, 0, array1.Length);
+                    stream.Flush();
+                }
+            }
+            else
+            {
+                Console.Write(line1);
+                Console.Write('\n');
+            }
+
+        }
+    }
+
+    interface ISorter {
+        void Sort<T>(T[] items) where T : IComparable;
+    }
+
+    public class MergeSort : ISorter {
+        private int lenght;
+        private void MergeSort_Recursive<T>(T[] numbers, T[] temp, int left, int right) where T : IComparable
+        {
+            int mid;
+            if (right > left)
+            {
+                mid = (right + left) / 2;
+                MergeSort_Recursive(numbers, temp, left, mid);
+                MergeSort_Recursive(numbers, temp, (mid + 1), right);
+
+                DoMerge(numbers, temp, left, (mid + 1), right);
+            }
+            return;
+        }
+        private void DoMerge<T>(T[] numbers, T[] temp, int left, int mid, int right) where T : IComparable
+        {
+            int left_end, num_elements, tmp_pos;
+
+            left_end = (mid - 1);
+            tmp_pos = left;
+            num_elements = (right - left + 1);
+
+            while ((left <= left_end) && (mid <= right))
+            {
+                if (numbers[left].CompareTo(numbers[mid]) <= 0)
+                    temp[tmp_pos++] = numbers[left++];
+                else
+                    temp[tmp_pos++] = numbers[mid++];
+            }
+
+            while (left <= left_end)
                 temp[tmp_pos++] = numbers[left++];
-            else
+
+            while (mid <= right)
                 temp[tmp_pos++] = numbers[mid++];
-        }
-
-        while (left <= left_end)
-            temp[tmp_pos++] = numbers[left++];
-
-        while (mid <= right)
-            temp[tmp_pos++] = numbers[mid++];
-        for (int i = 0; i < num_elements; i++)
-        {
-            numbers[right] = temp[right];
-            right--;
-        }
-    }
-
-    private static void MergeSort<T>(T[] items) where T : IComparable
-    {
-        length = items.Length;
-        T[] temp = new T[length];
-        MergeSort_Recursive(items, temp, 0, length - 1);
-    }
-    ////////////////////////////////////////////
-    private static void BubbleSort<T>(T[] source) where T : IComparable
-    {
-        length = source.Length;
-        bool sorted = false;
-
-        for (int iteration = 0; iteration <= length * length; iteration++) // Worst case is n^2
-        {
-            bool swapped = false;
-            for (int i = 0; i < length - 1; i++)
+            for (int i = 0; i < num_elements; i++)
             {
-                if (source [i]
-                        .CompareTo(source[i + 1]) == 1)
+                numbers[right] = temp[right];
+                right--;
+            }
+        }
+        public void Sort<T>(T[] items) where T : IComparable
+        {
+            lenght = items.Length;
+            var temp = new T[lenght];
+            MergeSort_Recursive(items, temp, 0, lenght - 1);
+        }
+    }
+
+    public class BubbleSort : ISorter {
+        private int lenght;
+        
+        private void Swap<T>(ref T a, ref T b) where T : IComparable
+        {
+            (a, b) = (b, a);
+        }
+        
+        ////////////////////////////////////////////
+        public void Sort<T>(T[] source) where T : IComparable
+        {
+            lenght = source.Length;
+            bool sorted = false;
+
+            for (int iteration = 0; iteration <= lenght * lenght; iteration++) // Worst case is n^2
+            {
+                bool swapped = false;
+                for (int i = 0; i < lenght - 1; i++)
                 {
-                    Swap(ref source[i], ref source[i + 1]);
-                    swapped = true;
+                    if (source [i]
+                            .CompareTo(source[i + 1]) == 1)
+                    {
+                        Swap(ref source[i], ref source[i + 1]);
+                        swapped = true;
+                    }
+                }
+
+                if (!swapped)
+                {
+                    sorted = true;
+                    break;
                 }
             }
-
-            if (!swapped)
-            {
-                sorted = true;
-                break;
-            }
-        }
-        if (!sorted)
-            throw new ArgumentException("Too few iterations to sort array.");
-    }
-    //////////////////////////////////////////
-    private static void ShellSort<T>(T[] array) where T : IComparable
-    {
-        int i, j, increment;
-        length = array.Length;
-        T temp;
-
-        increment = length / 2;
-        while (increment > 0)
-        {
-            for (i = 0; i < length; i++)
-            {
-                j = i;
-                temp = array[i];
-                while ((j >= increment) && (array [j - increment]
-                                                .CompareTo(temp) > 0))
-                {
-                    array[j] = array[j - increment];
-                    j = j - increment;
-                }
-                array[j] = temp;
-            }
-            if (increment == 2)
-                increment = 1;
-            else
-                increment = increment * 5 / 11;
+            if (!sorted)
+                throw new ArgumentException("Too few iterations to sort array.");
         }
     }
-    public static void SelectSort<T>(T[] arr) where T : IComparable
-    {
-        if (arr.Length <= 500)
+
+    public class ShellSort : ISorter {
+        private int lenght;
+        public void Sort<T>(T[] array) where T : IComparable
         {
-            Log.StartLog(arr, "BubbleSort");
-            BubbleSort(arr);
-            Log.EndLog(arr);
-        }
-        else
-        { // measure accuracy of sorted subarrays
-            int sorted_cnt = 0;
-            for (int i = 1; i < arr.Length; ++i)
+            lenght = array.Length;
+            T temp;
+
+            int increment = lenght / 2;
+            while (increment > 0)
             {
-                while (i < arr.Length && (arr [i]
-                                              .CompareTo(arr[i - 1]) >= 0))
+                for (int i = 0; i < lenght; i++)
                 {
-                    sorted_cnt++;
-                    i++;
+                    int j = i;
+                    temp = array[i];
+                    while ((j >= increment) && (array [j - increment]
+                                                    .CompareTo(temp) > 0))
+                    {
+                        array[j] = array[j - increment];
+                        j -= increment;
+                    }
+                    array[j] = temp;
                 }
+                if (increment == 2)
+                    increment = 1;
+                else
+                    increment *= 5 / 11;
             }
-            float accuracy = (float) sorted_cnt / (arr.Length - 1);
-            if (accuracy > 0.5 && arr.Length < 200000)
+        }
+        
+    }
+
+
+    public class SelectAlgorithm
+    {
+        
+        public static void SelectSort<T>(T[] arr, bool is_infile) where T : IComparable
+        {
+            ISorter a;
+            if (arr.Length <= 500)
             {
-                Log.StartLog(arr, "ShellSort");
-                ShellSort(arr);
-                Log.EndLog(arr);
+                a = new BubbleSort();
+                Log.StartLog(arr, "BubbleSort", is_infile);
+                a.Sort(arr);
+                Log.EndLog(arr, is_infile);
             }
             else
-            {
-                Log.StartLog(arr, "MergeSort");
-                MergeSort(arr);
-                Log.EndLog(arr);
+            { // measure accuracy of sorted subarrays
+                int sorted_cnt = 0;
+                for (int i = 1; i < arr.Length; ++i)
+                {
+                    while (i < arr.Length && (arr [i]
+                                                .CompareTo(arr[i - 1]) >= 0))
+                    {
+                        sorted_cnt++;
+                        i++;
+                    }
+                }
+                float accuracy = (float) sorted_cnt / (arr.Length - 1);
+                if (accuracy > 0.5 && arr.Length < 200000)
+                {
+                    a = new ShellSort();
+                    Log.StartLog(arr, "ShellSort", is_infile);
+                    a.Sort(arr);
+                    Log.EndLog(arr, is_infile);
+                }
+                else
+                {
+                    a = new MergeSort();
+                    Log.StartLog(arr, "MergeSort", is_infile);
+                    a.Sort(arr);
+                    Log.EndLog(arr, is_infile);
+                }
             }
         }
     }
-}
 
-public class Program
-{
-    // Generate of ints and strings have different algorithms(that's why function is not Generic)
-    private static int[] generateArray(int count)
+    public class Program
     {
-        Random random = new Random();
-        int[] values = new int[count];
-        for (int i = 0; i < count; ++i)
-            values[i] = random.Next();
-        return values;
-    }
-    private static string[] generateArrayOfString(int count)
-    {
-        string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        string[] values = new string[count];
-        string randString = "";
-        var random = new Random();
-        for (int j = 0; j < count; j++)
+        // Generate of ints and strings have different algorithms(that's why function is not Generic)
+        private static int[] generateArray(int count)
         {
-            for (int i = 0; i < 10; i++)
-            {
-                randString += chars[random.Next(chars.Length)];
-            }
-            values[j] = randString;
-            randString = "";
+            Random random = new Random();
+            int[] values = new int[count];
+            for (int i = 0; i < count; ++i)
+                values[i] = random.Next();
+            return values;
         }
-        return values;
-    }
-    public static void Main()
-    {
-        string[] randomStrings1 = generateArrayOfString(400);
-        SortAlgorithms.SelectSort(randomStrings1);
-        string[] randomStrings2 = generateArrayOfString(50000);
-        SortAlgorithms.SelectSort(randomStrings2);
+        private static string[] generateArrayOfString(int count)
+        {
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            string[] values = new string[count];
+            string randString = "";
+            var random = new Random();
+            for (int j = 0; j < count; j++)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    randString += chars[random.Next(chars.Length)];
+                }
+                values[j] = randString;
+                randString = "";
+            }
+            return values;
+        }
+        public static void Main()
+        {
+            Console.WriteLine("Where to write logs [f]ile/[c]onsole");
+            string input = Console.ReadLine();
+            bool is_infile = input[0] == 'f' ? true : false;
 
-        int[] randomNumbers1 = generateArray(80000);
-        SortAlgorithms.SelectSort(randomNumbers1);
-        int[] randomNumbers2 = generateArray(123200);
-        SortAlgorithms.SelectSort(randomNumbers2);
-        int[] randomNumbers3 = generateArray(1300000);
-        SortAlgorithms.SelectSort(randomNumbers3);
-        int[] randomNumbers4 = generateArray(6000000);
-        SortAlgorithms.SelectSort(randomNumbers4);
+            File.Delete("logs.txt");
+            string[] randomStrings1 = generateArrayOfString(400);
+            SelectAlgorithm.SelectSort(randomStrings1, is_infile);
+            string[] randomStrings2 = generateArrayOfString(50000);
+            SelectAlgorithm.SelectSort(randomStrings2, is_infile);
+
+            int[] randomNumbers1 = generateArray(80000);
+            SelectAlgorithm.SelectSort(randomNumbers1, is_infile);
+            int[] randomNumbers2 = generateArray(123200);
+            SelectAlgorithm.SelectSort(randomNumbers2, is_infile);
+            int[] randomNumbers3 = generateArray(1300000);
+            SelectAlgorithm.SelectSort(randomNumbers3, is_infile);
+            int[] randomNumbers4 = generateArray(6000000);
+            SelectAlgorithm.SelectSort(randomNumbers4, is_infile);
+        }
     }
-}
 }
