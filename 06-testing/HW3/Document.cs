2e1b9d8b3cdeca61ce460fdee7056d6e9ab70d0f
fayzullin <91489725+fayzullin@users.noreply.github.com>
2022-05-09 20:05:08 +0300

    Implemented `Document` class

---
+++ 06-testing/HW3/Document.cs
@@ -0,0 +1,25 @@
+namespace Testing.HW3;
+
+public class Document
+{
+    public string Title { get; init; }
+    public DateTime CreatedUtc { get; init; }
+    public string ClassName { get; init; }
+
+    public (List<Document>, List<Document>) SplitTrainTest(List<Document> documents, double trainSize, int? seed = null)
+    {
+        if (trainSize is >= 0 and <= 1)
+        {
+            throw new ArgumentException("trainSize " + trainSize + " must between 0 and 1");
+        }
+        
+        var sortedDocuments = documents
+            .OrderBy(document => document.Title)
+            .ThenBy(document => document.CreatedUtc);
+        var randomGenerator = seed == null ? new Random() : new Random(seed.Value);
+        var shuffledDocuments = sortedDocuments.OrderBy(_ => randomGenerator.Next()).ToList();
+        var trainCount = (int)(documents.Count * trainSize);
+        
+        return (shuffledDocuments.Take(trainCount).ToList(), shuffledDocuments.Skip(trainCount).ToList());
+    }
+}
