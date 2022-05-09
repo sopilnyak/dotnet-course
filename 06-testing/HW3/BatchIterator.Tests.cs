82992e0b8321075177c1d0575247688492febcfe
fayzullin <91489725+fayzullin@users.noreply.github.com>
2022-05-09 20:06:45 +0300

    Implemented `BatchIterator` class

---
+++ 06-testing/HW3/BatchIterator.Tests.cs
@@ -0,0 +1,78 @@
+using NUnit.Framework;
+
+namespace Testing.HW3;
+
+public class TestBatchIterator
+{
+    private static int CalcBatchSize(int dataSize, int batchSize, int batchIndex)
+    {
+        if (batchSize * (batchIndex + 1) < dataSize)
+        {
+            return batchSize;
+        }
+
+        return dataSize - batchSize * batchIndex;
+    }
+
+    private static int CalcElementIndex(int batchSize, int batchIndex, int batchElementIndex)
+    {
+        return batchSize * batchIndex + batchElementIndex;
+    }
+
+    [TestCase(new[] { 1, 2, 3, 4 }, 2, true)]
+    [TestCase(new[] { 1, 2, 3, 4 }, 2, false)]
+    [TestCase(new[] { 1, 2, 3, 4, 5, 6, 7 }, 3, true)]
+    [TestCase(new[] { 1, 2, 3, 4, 5, 6, 7 }, 3, true)]
+    [TestCase(new[] { 1, 2, 3, 4 }, 5, true)]
+    [TestCase(new[] { 1, 2, 3, 4 }, 5, false)]
+    [TestCase(new int[] { }, 3, true)]
+    [TestCase(new int[] { }, 3, false)]
+    [TestCase(new string[] { "1", "2", "3", "4", "5" }, 2, true)]
+    [TestCase(new string[] { "1", "2", "3", "4", "5" }, 2, false)]
+    public void BatchSizeTest<T>(IEnumerable<T> data, int batchSize, bool dropLast)
+    {
+        var dataList = data.ToList();
+        var batchIterator = new BatchIterator<T>(dataList, batchSize, dropLast);
+        var batchIndex = 0;
+
+        foreach (var batch in batchIterator)
+        {
+            Assert.AreEqual(
+                CalcBatchSize(dataList.Count, batchSize, batchIndex),
+                batch.Count());
+            ++batchIndex;
+        }
+    }
+
+    [TestCase(new[] { 1, 2, 3, 4 }, 2, true)]
+    [TestCase(new[] { 1, 2, 3, 4 }, 2, false)]
+    [TestCase(new[] { 1, 2, 3, 4, 5 }, 2, true)]
+    [TestCase(new[] { 1, 2, 3, 4, 5 }, 2, true)]
+    [TestCase(new[] { 1, 2, 3, 4 }, 5, true)]
+    [TestCase(new[] { 1, 2, 3, 4 }, 5, false)]
+    [TestCase(new int[] { }, 3, true)]
+    [TestCase(new int[] { }, 3, false)]
+    [TestCase(new string[] { "1", "2", "3", "4", "5" }, 2, true)]
+    [TestCase(new string[] { "1", "2", "3", "4", "5" }, 2, false)]
+    public void BatchDataTest<T>(IEnumerable<T> data, int batchSize, bool dropLast)
+    {
+        var dataList = data.ToList();
+        var batchIterator = new BatchIterator<T>(dataList, batchSize, dropLast);
+        var batchIndex = 0;
+
+        foreach (var batch in batchIterator)
+        {
+            var batchList = batch.ToList();
+            var batchElementIndex = 0;
+            foreach (var elem in batchList)
+            {
+                Assert.AreEqual(
+                    dataList[CalcElementIndex(batchSize, batchIndex, batchElementIndex)],
+                    elem);
+                ++batchElementIndex;
+            }
+
+            ++batchIndex;
+        }
+    }
+}
