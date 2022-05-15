77f0e38101547997f2d1e8e28634026edd42cfba
Tigran <tigrankoshkelyan@gmail.com>
2022-05-15 19:16:46 +0300

    Seed

--- 06-testing/Solution/Document.cs
+++ 06-testing/Solution/Document.cs
@@ -5,14 +5,14 @@ public class Document
     public string Title { get; init; }
     public DateTime CreatedUtc { get; init; }
     public string ClassName { get; init; }
-    
+
     public static (List<Document>, List<Document>) SplitTrainTest(List<Document> documents, double trainSize)
     {
         var sortedDocuments = documents
             .OrderBy(document => document.Title)
             .ThenBy(document => document.CreatedUtc);
 
-        var random = new Random();
+        var random = new Random(239);
         var randomDocuments = sortedDocuments.OrderBy(_ => random.Next()).ToList();
 
         var trainAbsSize = (int) (documents.Count * trainSize);
